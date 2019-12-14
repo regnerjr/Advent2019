6201facb48c1bd51001ce039219636520c1d0068
John Regner <john.regner@walmartlabs.com>
2019-12-14 14:39:09 -0800

    Advent of code day 2 working.
    Why:

--- lib/program_alarm.rb
+++ lib/program_alarm.rb
@@ -9,35 +9,31 @@
 
 # Add our values get 70, then store it at position 3 ( 0, 1, 2, 3 )
 
-class Program_Alarm
-
+class ProgramAlarm
   @codes = []
-  @offset = 0
+  @instruction_pointer = 0
 
   def process(program_input)
     @codes = program_input.split(',')
-    @offset = 0
+    @instruction_pointer = 0
 
-    while @offset < @codes.count
+    while @instruction_pointer < @codes.count
 
-      opcode = @codes[@offset].to_i
+      opcode = @codes[@instruction_pointer].to_i
+      return @codes.join(',') if opcode == 99
 
-      if opcode == 99
-        return @codes.join(',')
-      end
-
-      updated_value = compute(opcode, @offset)
-      result_address = fetch_result_address(@offset)
+      updated_value = compute(opcode, @instruction_pointer)
+      result_address = fetch_result_address(@instruction_pointer)
 
       @codes[result_address] = updated_value
-      @offset += 4
+      @instruction_pointer += 4
     end
   end
 
   private
 
-  def compute(opcode, offset)
-    address1, address2 = fetch_addresses(offset)
+  def compute(opcode, instruction_pointer)
+    address1, address2 = fetch_addresses(instruction_pointer)
     value1, value2 = extract_values(address1, address2)
 
     case opcode
@@ -55,13 +51,13 @@ class Program_Alarm
     [value1, value2]
   end
 
-  def fetch_addresses(offset)
-    address1 = @codes[offset + 1].to_i
-    address2 = @codes[offset + 2].to_i
+  def fetch_addresses(instruction_pointer)
+    address1 = @codes[instruction_pointer + 1].to_i
+    address2 = @codes[instruction_pointer + 2].to_i
     [address1, address2]
   end
 
-  def fetch_result_address(offset)
-    @codes[offset + 3].to_i
+  def fetch_result_address(instruction_pointer)
+    @codes[instruction_pointer + 3].to_i
   end
 end
--- test/program_alarm_tests.rb
+++ test/program_alarm_tests.rb
@@ -6,7 +6,7 @@ require 'program_alarm'
 
 class ProgramAlarmTest < Minitest::Test
   def test_example_1
-    alarm = Program_Alarm.new
+    alarm = ProgramAlarm.new
     program = '1,9,10,3,2,3,11,0,99,30,40,50'
     result = alarm.process(program)
     expected = '3500,9,10,70,2,3,11,0,99,30,40,50'
@@ -14,7 +14,7 @@ class ProgramAlarmTest < Minitest::Test
   end
 
   def test_example_2
-    alarm = Program_Alarm.new
+    alarm = ProgramAlarm.new
     program = '1,0,0,0,99'
     result = alarm.process(program)
     expected = '2,0,0,0,99'
@@ -22,7 +22,7 @@ class ProgramAlarmTest < Minitest::Test
   end
 
   def test_example_3
-    alarm = Program_Alarm.new
+    alarm = ProgramAlarm.new
     program = '2,3,0,3,99'
     result = alarm.process(program)
     expected = '2,3,0,6,99'
@@ -30,7 +30,7 @@ class ProgramAlarmTest < Minitest::Test
   end
 
   def test_example_4
-    alarm = Program_Alarm.new
+    alarm = ProgramAlarm.new
     program = '2,4,4,5,99,0'
     result = alarm.process(program)
     expected = '2,4,4,5,99,9801'
@@ -38,7 +38,7 @@ class ProgramAlarmTest < Minitest::Test
   end
 
   def test_example_5
-    alarm = Program_Alarm.new
+    alarm = ProgramAlarm.new
     program = '1,1,1,4,99,5,6,0,99'
     result = alarm.process(program)
     expected = '30,1,1,4,2,5,6,0,99'
